cd6e20a9eea4236acc19198bd07cb349124a326b
Emma-Nangida <emmanangida@gmail.com>
2023-02-15 15:56:04 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+import com.sun.jdi.NativeMethodException
+
+fun main() {
+    var name= "akirachix"
+    println(name[0])
+    println(name [2])
+    println(name [3])
+    println(sentence("Emma",23))
+    println("Marsabit")
+    println("i don't know you")
+
+
+
+
+
+    }
+
+fun sentence(name: String,age: Int): String{
+    return "Hi, i am $name and i am $age years old"
+    }
+
+
+fun length(name: String) :Int {
+    return length("county")
+}
+fun statement(name: String){
+    var name = "Winner"
+    if(name == "pretty")
+        println("That's me")
+
+    }
+
+
+
+
+
+
+
